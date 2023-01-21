33c703b498e731cd58c99efcf80fdfc9a449a524
Keith Burroughs <51885619+keithhb33@users.noreply.github.com>
2023-01-21 17:31:35 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ai_images',
-    version='0.6',
+    version='0.7',
     author="Keith Burroughs",
     author_email='keithburroughs33@gmail.com',
     packages=find_packages('src'),
