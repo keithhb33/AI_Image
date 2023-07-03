dd3a638a94c893c7276c66e25c2d74f840c72e65
Keith Burroughs <51885619+keithhb33@users.noreply.github.com>
2023-07-03 18:37:28 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
 from setuptools import setup, find_packages
 
-
 setup(
     name='ai_images',
-    version='0.7',
+    version='0.1.1',
     author="Keith Burroughs",
     author_email='keithburroughs33@gmail.com',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
+    packages=find_packages(),
     url='https://github.com/keithhb33/AI-Image-Bot/',
-    keywords='AI Images',
+    keywords=["python", "images", "ai", "ai generated", "movies", "photography", "modules"],
     install_requires=[
-          'ai-image-bot',
+          'requests'
       ],
 
 )
